7034e673726ba4d848d58784dada86ef40387341
lisenko <lix7@me.com>
2022-01-23 03:23:24 +0300

    23/256 adc/sbc

--- 6502.c
+++ 6502.c
@@ -15,6 +15,17 @@ I	Interrupt (IRQ disable)
 Z	Zero
 C	Carry */
 
+uint8_t *_rom;
+struct cpu *_cpu;
+
+uint8_t op1() {
+    return _rom[_cpu->pc + 1];
+}
+
+uint8_t op2() {
+    return _rom[_cpu->pc + 1];
+}
+
 void flags_zn(struct cpu *cpu, uint8_t val) {
     if (val == 0) {
         cpu->sr &= ~0b10;
@@ -24,8 +35,76 @@ void flags_zn(struct cpu *cpu, uint8_t val) {
     }
 }
 
+void clc(struct cpu *cpu) {
+    cpu->sr &= ~0b1;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void sec(struct cpu *cpu) {
+    cpu->sr |= 0b1;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void cli(struct cpu *cpu) {
+    cpu->sr &= ~0b100;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void sei(struct cpu *cpu) {
+    cpu->sr |= 0b100;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void clv(struct cpu *cpu) {
+    cpu->sr &= ~0b1000000;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void cld(struct cpu *cpu) {
+    cpu->sr &= ~0b1000;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
+void sed(struct cpu *cpu) {
+    cpu->sr |= 0b1000;
+    cpu->pc++;
+    cpu->cycles += 2;
+}
+
 void lda(struct cpu *cpu, uint8_t byte) {
     cpu->a = byte;
+    //update pc in opcode
+    //update cycles in opcode
+    flags_zn(cpu, cpu->a);
+};
+
+void adc(struct cpu *cpu, uint8_t byte) {
+    uint16_t result = cpu->a + byte + (cpu->sr & 1 ? 1 : 0);
+    // TODO V
+    if (result > 256) {
+        cpu->sr |= 0b1;
+        result -= 256;
+    }
+    cpu->a = (uint8_t)result;
+    //update pc in opcode
+    //update cycles in opcode
+    flags_zn(cpu, cpu->a);
+};
+
+void sbc(struct cpu *cpu, uint8_t byte) {
+    uint16_t result = 0xFF + cpu->a - byte + (cpu->sr & 1 ? 0 : 1);
+    // TODO V
+    if (result > 256) {
+        cpu->sr |= 0b1;
+        result -= 256;
+    }
+    cpu->a = (uint8_t)result;
     //update pc in opcode
     //update cycles in opcode
     flags_zn(cpu, cpu->a);
@@ -61,30 +140,42 @@ void iny(struct cpu *cpu) {
 
 char * step(struct cpu *cpu, uint8_t opcode, uint8_t rom[]) {
     char *msg = malloc(100);
+    _cpu = cpu;
+    _rom = rom;
     switch(opcode) {
         case 0x00: // BRK
             //TODO CYKA
             return "BRK";
         case 0x18: // CLC
-            cpu->sr &= ~0b1;
-            cpu->pc++;
-            cpu->cycles += 2;
+            clc(cpu);
             return "CLC";
         case 0x38: // SEC
-            cpu->sr |= 0b1;
-            cpu->pc++;
-            cpu->cycles += 2;
+            sec(cpu);
             return "SEC";
         case 0x58: // CLI
-            cpu->sr &= ~0b100;
-            cpu->pc++;
-            cpu->cycles += 2;
+            cli(cpu);
             return "CLI";
-        case 0x78: // SEI
-            cpu->sr |= 0b100;
-            cpu->pc++;
+        case 0x65: // ADC zp
+            adc(cpu, memr(cpu, op1()));
+            snprintf(msg, 100, "ADC $%X", op1());
+            cpu->pc += 2;
             cpu->cycles += 2;
+            return msg;
+        case 0x69: // ADC imm
+            adc(cpu, op1());
+            snprintf(msg, 100, "ADC $#%X", op1());
+            cpu->pc += 2;
+            cpu->cycles += 2;
+            return msg;
+        case 0x78: // SEI
+            sei(cpu);
             return "SEI";
+        case 0x85: // STA zp
+            memw(cpu, op1(), cpu->a);
+            snprintf(msg, 100, "STA $%X", op1());
+            cpu->pc += 2;
+            cpu->cycles += 3;
+            return msg;
         case 0x8A: // TXA
             trans(cpu, &cpu->x, &cpu->a);
             return "TXA";
@@ -98,14 +189,14 @@ char * step(struct cpu *cpu, uint8_t opcode, uint8_t rom[]) {
             trans(cpu, &cpu->a, &cpu->y);
             return "TAY";
         case 0xA5: // LDA zp
-            lda(cpu, memr(cpu, rom[cpu->pc + 1]));
-            snprintf(msg, 100, "LDA $%X", rom[cpu->pc + 1]);
+            lda(cpu, memr(cpu, op1()));
+            snprintf(msg, 100, "LDA $%X", op1());
             cpu->pc += 2;
             cpu->cycles += 3;
             return msg;
         case 0xA9: // LDA imm
-            lda(cpu, rom[cpu->pc + 1]);
-            snprintf(msg, 100, "LDA #%X", rom[cpu->pc + 1]);
+            lda(cpu, op1());
+            snprintf(msg, 100, "LDA $#%X", op1());
             cpu->pc += 2;
             cpu->cycles += 2;
             return msg;
@@ -113,22 +204,20 @@ char * step(struct cpu *cpu, uint8_t opcode, uint8_t rom[]) {
             trans(cpu, &cpu->a, &cpu->x);
             return "TAX";
         case 0xAD: // LDA abs
-            uint16_t addr = (rom[cpu->pc + 2] << 8) + rom[cpu->pc + 1];
+            uint16_t addr = (op2() << 8) + op1();
             lda(cpu, memr(cpu, addr));
-            snprintf(msg, 100, "LDA $%X%X", rom[cpu->pc + 2], rom[cpu->pc + 1]);
+            snprintf(msg, 100, "LDA $%X%X", op2(), op1());
             cpu->pc += 3;
             cpu->cycles += 4;
             return msg;
         case 0xB5: // LDA zp+x
-            lda(cpu, memr(cpu, rom[((cpu->pc + 1) + cpu->x) % 256]));
-            snprintf(msg, 100, "LDA $%X", rom[cpu->pc + 1]);
+            lda(cpu, memr(cpu, rom[(op1() + cpu->x) % 256]));
+            snprintf(msg, 100, "LDA $%X", op1());
             cpu->pc += 2;
             cpu->cycles += 4;
             return msg;
         case 0xB8: // CLV
-            cpu->sr &= ~0b1000000;
-            cpu->pc++;
-            cpu->cycles += 2;
+            clv(cpu);
             return "CLV";
         case 0xBA: // TSX
             trans(cpu, &cpu->sp, &cpu->x);
@@ -137,21 +226,19 @@ char * step(struct cpu *cpu, uint8_t opcode, uint8_t rom[]) {
             iny(cpu);
             return "INY";
         case 0xD8: // CLD
-            cpu->sr &= ~0b1000;
-            cpu->pc++;
-            cpu->cycles += 2;
+            cld(cpu);
             return "CLD";
         case 0xE8: // INX
             inx(cpu);
             return "INX";
+        case 0xE9: // SBC imm
+        
         case 0xEA: // NOP
             cpu->pc++;
             cpu->cycles += 2;
             return "NOP";
         case 0xF8: // SED
-            cpu->sr |= 0b1000;
-            cpu->pc++;
-            cpu->cycles += 2;
+            sed(cpu);
             return "SED";
         default:
             snprintf(msg, 100, "Unknown opcode %X", opcode);
